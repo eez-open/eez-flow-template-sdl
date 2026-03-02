8bb69c045706295d9e4c8406544b382593ef7cb4
Martin Vladić <martin.vladic@gmail.com>
2026-03-02 15:30:29 +0100

    #1: update for the latest eez-framework

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,9 @@ uint32_t DISPLAY_HEIGHT = 272;
 
 void init() {
     eez::initAssetsMemory();
-    eez::gui::loadMainAssets(eez::gui::assets, sizeof(eez::gui::assets));
-    DISPLAY_WIDTH = eez::gui::g_mainAssets->settings->displayWidth;
-    DISPLAY_HEIGHT = eez::gui::g_mainAssets->settings->displayHeight;
+    eez::loadMainAssets(eez::gui::assets, sizeof(eez::gui::assets));
+    DISPLAY_WIDTH = eez::g_mainAssets->settings->displayWidth;
+    DISPLAY_HEIGHT = eez::g_mainAssets->settings->displayHeight;
     eez::initOtherMemory();
     eez::initAllocHeap(eez::ALLOC_BUFFER, eez::ALLOC_BUFFER_SIZE);
 
